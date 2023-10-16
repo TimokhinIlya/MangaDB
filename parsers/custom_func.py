789ee6b7961b71c16a1e2567fce00846ec1ae09a
ilya.timokhin <prtnr.ilya.timokhin@rtmis.ru>
2023-10-16 11:33:45 +0500

    парсер манги для remanga.org

--- parsers/custom_func.py
+++ parsers/custom_func.py
@@ -1,50 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
 from typing import Optional
+import re
+import json
 
 def get_manga_link(manga_name: str) -> Optional[str]: # Функция получения ссылки на страницу с искомой мангой
 
-    #   URL главной страницы
-    url = "https://remanga.org/manga"
+    json_url = f'https://api.remanga.org/api/search/?query={manga_name}&count=5&field=titles'
+    # Получение JSON-данных из ссылки
+    response = requests.get(json_url)
+    data = response.json()
 
-    # Отправка GET-запроса и получение HTML-страницы
-    response = requests.get(url)
+    eng_name = None
 
-    try:
-        response.raise_for_status() # Проверка наличия ошибок при запросе
-        
-        # Разбор HTML-страницы с помощью Beautiful Soup
-        soup = BeautifulSoup(response.text, "lxml")
-
-
-        # Извлечение заголовка манги
-        element = soup.find('a', {'title': manga_name})
-
-        if element:
-        # Получите значение атрибута href
-            href = element['href']
-            # Получаем ссылку на страницу искомой манги
-            manga_url = f'https://remanga.org{href}'
-            return manga_url
-        else:
-            return f'Манга с названием "{manga_name}" не найдена'
-    except requests.exceptions.RequestException as e:
-        return f'Ошибка при отправке запроса: {e}'
-    except Exception as e:
-        return f'Произошла ошибка: {e}'
+    for content in (data['content']):
+        if content['main_name'] == manga_name:
+            eng_name = content['dir']
+            break
+    manga_url = f"https://remanga.org/manga/{eng_name.replace(' ','-')}"
+    return manga_url
     
-def get_manga_chapter(manga_url:str) -> Optional[str]: # Получаем ссылку из функции get_manga_link
+manga_url = get_manga_link('Наруто')
+print(manga_url)
 
-    #   URL страницы с мангой
-    response = requests.get(manga_url + '?p=content')
-    
-    response.raise_for_status() # Проверка наличия ошибок при запросе
-    
+def get_manga_id(manga_url:str) -> Optional[str]: # Получаем id манги
+
+    # URL страницы с мангой
+    response = requests.get(manga_url)
+
     # Разбор HTML-страницы с помощью Beautiful Soup
     soup = BeautifulSoup(response.text, "lxml")
 
-    # Извлечение последней вышедшей главы манги
-    element = soup.find('p', {'class': 'Typography_body1__YTqxB Typography_color-inherit__Wstd_ Chapters_title__ocJer'})
-    return element
+    script_tag = soup.find("script", string=lambda x: "branches" in str(x))
 
-print(get_manga_link('Легенда о северном клинке'))
+    json_data = re.search(r'({.*})', str(script_tag)).group(0)
+
+    data_dict = json.loads(json_data)
+
+    # Извлечение branches id
+    branches_id = data_dict['props']['pageProps']['fallbackData']['content']['branches'][0]['id']
+
+    return branches_id
+
+manga_id = get_manga_id(manga_url)
+print(manga_id)
+
+def get_manga_chapter(manga_id)-> Optional[int]:
+    
+    json_url = f'https://api.remanga.org/api/titles/chapters/?branch_id={manga_id}&ordering=-index&user_data=1&count=40&page=1'
+    # Получение JSON-данных из ссылки
+    response = requests.get(json_url)
+    data = response.json()
+
+    last_chapter_with_null_price = None
+
+    for content in (data['content']):
+        if not content['is_paid']:
+            last_chapter_with_null_price = content['chapter']
+            break
+    return last_chapter_with_null_price
+
+print(get_manga_chapter(manga_id))
---
+++ parsers/html/test.py
@@ -0,0 +1,22 @@
+import re
+import json
+from bs4 import BeautifulSoup
+
+with open("parsers\html\manga_page_with_params.html", "r", encoding="utf-8") as file:
+    html_content = file.read()
+
+soup = BeautifulSoup(html_content, "lxml")
+
+script_tag = soup.find("script", string=lambda x: "branches" in str(x))
+
+json_data = re.search(r'({.*})', str(script_tag)).group(0)
+
+data_dict = json.loads(json_data)
+
+# Извлечение branches id
+branches_id = data_dict['props']['pageProps']['fallbackData']['content']['branches'][0]['id']
+
+print(branches_id)
+
+
+
